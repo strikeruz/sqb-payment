b7eab806bb3d2d21786d05849afdea934bedbe5e
Nursulton <nk@77projects.com>
2021-02-11 20:16:52 +0500

    added field generator and add func for validations

--- src/components/p2p/Forms/FormElements/FormInputItem.js
+++ src/components/p2p/Forms/FormElements/FormInputItem.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
-const FormInputItem = ({label, type, name, placeholder, className, register, error}) => {
+const FormInputItem = ({label, type, name, placeholder, className, register, error, customValidate}) => {
+    const defaultSchema = {
+        required: true, 
+        minLength: 3
+    }
+    const schema = Object.assign(defaultSchema, customValidate);
+    
     return (
         <div className="inline-list--second__item--popup--item">
             <label>{ label }</label>
             <input
-                ref={register}
+                ref={register(schema)}
                 name={name} 
                 type={type}
                 placeholder={placeholder} 
--- src/components/p2p/Payment.js
+++ src/components/p2p/Payment.js
@@ -4,10 +4,10 @@ import {
     Switch,
     Route
 } from "react-router-dom";
-import Categories from './Categories';
-import Services from './Services';
-import ModalWrapper from './ModalWrapper'
-import FirstFormStep from './Steps/FirstFormStep';
+import Categories from './PaymentCategories/Categories';
+import Services from './PaymentServices/Services';
+import ModalContainer from './ModalContainer'
+import FirstFormStep from './Forms/Steps/FirstFormStep';
 
 export default function Payment() {
     return (
@@ -16,7 +16,7 @@ export default function Payment() {
                 <Switch>
                     <Route path="/" render={() => <Categories/>} exact />
                     <Route path="/step2" render={() => <Services /> } />
-                    <Route path="/step3" render={() => <ModalWrapper><FirstFormStep /></ModalWrapper>} />
+                    <Route path="/step3" render={() => <ModalContainer><FirstFormStep /></ModalContainer>} />
                     <Route path="/step4" render={() => <Categories />} />
                 </Switch>
             </Router>
